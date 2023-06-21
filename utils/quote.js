fadf7b442fdb427345fc5a656719a1e88df2cd22
Coder-III <coderj3@proton.me>
2023-06-21 14:13:52 -0700

    Added quote template. Removed commented out function code.

---
+++ utils/quote.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        text: "It is, as some admiral once said, a trap.",
+        attribution: "Some Admiral",
+    },
+    {
+        text: "Which hand of the Witch hand will it be? AhHaHeeHooHooAhAhHah!",
+        attribution: "",
+    },
+    {
+        text: "You can't hear a whisper if you're constantly shouting.",
+        attribution: "",
+    },
+    {
+        text: "Buffalo buffalo buffalo Buffalo buffalo buffalo Buffalo buffalo." ,
+        attribution: "Buffalo buffalo",
+    },
+    {
+        text: "A nickle ain't worth a dime anymore.",
+        attribution: "Yogi Berra",
+    },
+    {
+        text: "I always knew I was a star. And now, the rest of the world seems to agree with me.",
+        attribution: "Freddie Mercury",
+    },
+    {
+        text: "That panda hit me for a full fucking heart.",
+        attribution: "Knotty",
+    },
+    {
+        text: "I don't understand anything anymore...I'm too tired... I'll stay here with my friend the floor, he's very cold and we understand each other...",
+        attribution: "anonymous",
+    },
+]
